768cb208689f59dfe6b4a4aab99ae11e4253c9eb
Guillermo San Martín <guille.seba0802@gmail.com>
2023-04-25 18:22:04 -0300

    fetchApi, videoDetail

--- src/utils/fetchApi.js
+++ src/utils/fetchApi.js
@@ -4,7 +4,7 @@ const BASE_URL = "https://youtube-v31.p.rapidapi.com";
 
 const options = {
   params: {
-    maxResults: "5",
+    maxResults: "20",
   },
   headers: {
     "X-RapidAPI-Key": import.meta.env.VITE_API_KEY,
